0ba6c53083a13b345135146c05ce92746a22c43c
Mykty-Ayan <akkassov.ayan@gmail.com>
2020-07-08 03:45:40 +0600

    Added sql statements and froze venv

---
+++ project1/sql/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE fligths (
+id SERIAL PRIMARY KEY,
+origin VARCHAR NOT NULL,
+destination VARCHAR NOT NULL,
+duration INTEGER NOT NULL
+);
---
+++ project1/sql/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO fligths (origin, destination, duration) VALUES ('New-York', 'London', 415);
+INSERT INTO fligths (origin, destination, duration) VALUES ('Shanghai', 'Paris', 760);
+INSERT INTO fligths (origin, destination, duration) VALUES ('Istanbul', 'Tokyo', 700);
+INSERT INTO fligths (origin, destination, duration) VALUES ('New-York', 'Paris', 435);
+INSERT INTO fligths (origin, destination, duration) VALUES ('Moscow', 'Paris', 245);
+INSERT INTO fligths (origin, destination, duration) VALUES ('Lima', 'New-York', 455);
+
